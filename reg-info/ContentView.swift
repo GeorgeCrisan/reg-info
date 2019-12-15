000080539100a8d57ef5058e88d79ff9c38f0793
GeorgeCrisan <georgerdp@gmail.com>
2019-12-15 22:59:40 +0000

    chore: add first view and background image

--- reg-info/ContentView.swift
+++ reg-info/ContentView.swift
@@ -8,9 +8,35 @@
 
 import SwiftUI
 
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var reg: String = "";
+    
+    let appBlue = UIColor(r: 0, g: 142, b: 207)
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("bgimage")
+            VStack {
+                TextField("Enter Vehicle REG", text: $reg).foregroundColor(Color(appBlue))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .fixedSize()
+
+                Text("\(reg.uppercased())")
+                    .font(.custom( "Gill Sans", size: 36))
+                    .foregroundColor(Color(appBlue))
+            
+                
+            }
+        }
+        
     }
 }
 
