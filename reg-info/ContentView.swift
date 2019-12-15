d0d4d44c76792e93376b6fa8cead97a95a9a19bc
GeorgeCrisan <georgerdp@gmail.com>
2019-12-15 19:44:08 +0000

    Initial Commit

---
+++ reg-info/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  reg-info
+//
+//  Created by George Crisan on 15/12/2019.
+//  Copyright © 2019 George Crisan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
